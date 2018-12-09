667c1e7f9a2038caa63a252d3b876e9c1b0af511
David Collins <dave99collins@gmail.com>
2018-12-08 23:53:56 -0500

    Add project files.

---
+++ TypingTest/Default.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TypingTest.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .auto-style2 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <h1 class="auto-style2"><strong>Test your typing speed!</strong></h1>
+        </div>
+        <h3>When ready to start, press the start button.</h3>
+        <h3 style="font-family: Arial, Helvetica, sans-serif">Enter Number of Rounds to Play:&nbsp;
+            <asp:DropDownList ID="DropDownList" runat="server" Height="44px" style="font-family: Arial, Helvetica, sans-serif" Width="82px" OnSelectedIndexChanged="DropDownList_SelectedIndexChanged">
+                <asp:ListItem>1</asp:ListItem>
+                <asp:ListItem Value="2"></asp:ListItem>
+                <asp:ListItem Value="3"></asp:ListItem>
+                <asp:ListItem>4</asp:ListItem>
+                <asp:ListItem>5</asp:ListItem>
+            </asp:DropDownList>
+        </h3>
+        <p>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+            <asp:Button ID="startBtn" runat="server" OnClick="startBtn_Click" style="font-family: Arial, Helvetica, sans-serif; font-weight: 700" Text="Start" />
+        </p>
+        <p>
+            <asp:Label ID="shownTextLabel" runat="server" Height="193px" style="font-family: Arial, Helvetica, sans-serif" Width="851px"></asp:Label>
+        </p>
+        <hr />
+        <p class="auto-style1">
+            <strong>Type the above text in the box below:&nbsp;</strong></p>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <span class="auto-style1">Words Per Minute:&nbsp; </span>
+                <asp:Label ID="wpmLabel" runat="server" CssClass="auto-style1" Text="0"></asp:Label>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class="auto-style1">Time Elapsed:&nbsp; </span>
+                <asp:Label ID="timeLabel" runat="server" CssClass="auto-style1" Text="0"></asp:Label>
+                <span class="auto-style1">s&nbsp;&nbsp;&nbsp;&nbsp; </span>
+                <asp:Timer ID="Timer" runat="server" Interval="1000" OnTick="Timer_Tick">
+                </asp:Timer>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+        <br />
+        <asp:Panel ID="Panel1" runat="server" DefaultButton="doneBtn">
+            
+                <ContentTemplate>
+                    <asp:TextBox ID="goodInputTB" runat="server" Height="41px" Width="1225px"></asp:TextBox>
+                    <br />
+                    <asp:Button ID="doneBtn" runat="server" Height="63px" OnClick="doneBtn_Click" Text="Done!" Width="172px" />
+                </ContentTemplate>
+            
+        </asp:Panel>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <h2>
+            <asp:Label ID="finishLabel" runat="server"></asp:Label>
+        </h2>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ TypingTest/Default.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TypingTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack) //only load the file the first time
+            {
+                Timer.Enabled = false;
+                ViewState["lines"] = File.ReadAllLines(Server.MapPath("typeText.txt"));
+                string[] temp = File.ReadAllLines(Server.MapPath("typeText.txt"));
+                ViewState["length"] = temp.Length;
+                ViewState["correctWords"] = 0;
+                ViewState["wpm"] = 0;
+                ViewState["numSeconds"] = 0;
+                ViewState["numberRounds"] = int.Parse(DropDownList.SelectedValue);
+            }
+            
+            
+        }
+
+        protected void Timer_Tick(object sender, EventArgs e)
+        {
+            int numSec = int.Parse(ViewState["numSeconds"].ToString());
+            numSec++;
+            ViewState["numSeconds"] = numSec;
+            timeLabel.Text = ViewState["numSeconds"].ToString();
+            //goodInputTB.Focus(); not good
+        }
+
+
+        private void roundFinished()
+        {
+            ViewState["numberRounds"] = int.Parse(ViewState["numberRounds"].ToString()) - 1;
+            if (int.Parse(ViewState["numberRounds"].ToString())>0)
+            {
+                shownTextLabel.Text = findRandomText();
+                goodInputTB.Focus(); //move the keyboard input directly to this text box
+            } else
+            {
+                wpmLabel.Text = ViewState["wpm"].ToString();
+                Timer.Enabled = false;
+                //all done
+                finishLabel.Text = "Congratulations! To play again, press start at the top.";
+                ViewState["correctWords"] = 0;
+                ViewState["wpm"] = 0;
+                ViewState["numSeconds"] = 0;
+            }
+            
+        }
+
+        protected void startBtn_Click(object sender, EventArgs e)
+        {
+            shownTextLabel.Text = findRandomText();
+            goodInputTB.Focus(); //move the keyboard input directly to this text box
+            Timer.Enabled = true;
+            finishLabel.Text = "";
+        }
+
+        private string findRandomText()
+        {
+
+            Random rand = new Random((int)DateTime.Now.Ticks);
+            int r = rand.Next((int)ViewState["length"]);
+            string[] temp = ViewState["lines"] as string[];
+            return temp[r];
+
+        }
+
+        //called by default through the panel when user presses the enter key!
+        protected void doneBtn_Click(object sender, EventArgs e)
+        {
+
+            string[] matchText = shownTextLabel.Text.Split(' ');
+            string[] inputText = goodInputTB.Text.Split(' ');
+            int currentMatch = 0;
+            for (int i=0; i<inputText.Length; i++)
+            {
+                if (i >= matchText.Length)
+                    break;
+                if (inputText[i].Equals(matchText[i])) {
+                    currentMatch++;
+                }
+            }
+
+          
+            ViewState["correctWords"] = int.Parse(ViewState["correctWords"].ToString()) + currentMatch;
+            currentMatch = 0;
+            int numSec = int.Parse(ViewState["numSeconds"].ToString());
+            int correctWords = int.Parse(ViewState["correctWords"].ToString());
+            int wpm = (int)(((double)correctWords / (double)numSec) * 60);
+            ViewState["wpm"] = wpm.ToString();
+          
+
+            goodInputTB.Text = "";
+            shownTextLabel.Text = "";
+            wpmLabel.Text = wpm.ToString();
+
+            roundFinished();
+        }
+
+        protected void DropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ViewState["numberRounds"] = int.Parse(DropDownList.SelectedValue);
+        }
+    }
+}
